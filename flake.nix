91a8099504926a7de75a817b5cc20476a9d5789c
yuanw <me@yuanwang.ca>
2022-02-25 12:38:47 -0600

    setup project

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "virtual environments";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gitignore = {
+    url = "github:hercules-ci/gitignore.nix";
+    # Use the same nixpkgs
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, devshell, flake-utils, gitignore }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlay
+            (final: prev: {
+              haskellPackages = prev.haskellPackages.override {
+                overrides = hself: hsuper: {
+                  wordle = hself.callCabal2nix "wordle"
+                    (gitignore.lib.gitignoreSource ./.) { };
+                };
+              };
+              wordle = final.haskell.lib.justStaticExecutables
+                final.haskellPackages.wordle;
+            })
+          ];
+        };
+        myHaskellEnv = (pkgs.haskellPackages.ghcWithHoogle (p:
+          with p; [
+            wordle
+            cabal-install
+            ormolu
+            hlint
+            hpack
+            brittany
+          ]));
+
+      in {
+        packages = { wordle = pkgs.wordle; };
+        defaultPackage = pkgs.wordle;
+        checks = self.packages;
+        devShell = pkgs.devshell.mkShell {
+          name = "wordle";
+          imports = [ (pkgs.devshell.extraModulesDir + "/git/hooks.nix") ];
+          git.hooks.enable = true;
+          git.hooks.pre-commit.text = "${pkgs.treefmt}/bin/treefmt";
+          packages = [ myHaskellEnv pkgs.treefmt pkgs.nixfmt ];
+        };
+      });
+}
