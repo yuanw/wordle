ec91c79112186b05eeea02a3f549d5cdd14ff89d
yuanw <me@yuanwang.ca>
2022-02-26 14:18:05 -0600

    update

--- src/TH_Lift.hs
+++ src/TH_Lift.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE TemplateHaskell #-} -- Enable TH
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE DeriveLift #-}
-
+{-# LANGUAGE FlexibleInstances #-}
 module TH_Lift  where
 
 import Language.Haskell.TH.Syntax ( Lift )
 
 import Data.IntSet.Internal ( IntSet(..) )
-
 deriving instance Lift (IntSet)
